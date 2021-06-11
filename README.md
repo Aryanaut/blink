7356d7581ec5a5427d8a40af2a218739758ab7e1
Aryan Mahesh <aryan.mahesh2010@gmail.com>
2021-06-11 11:36:02 +0530

    --first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # blink
 A program to translate your blinks into Morse code which then translates into letters. 
+
+# Morse Template
+![](morse-code.png)
---
+++ version_1.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np 
+import dlib 
+from scipy.spatial import distance as dist
+
+cap = cv2.VideoCapture(0) 
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+def midpoint(p1 ,p2):
+    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray)
+    rectangles = str(faces)
+    # frame = cv2.resize(frame, None, fx=0.5, fy=0.5)
+    if rectangles == 'rectangles[]':
+        pass
+    else:
+        for face in faces:
+            x, y = face.left(), face.top()
+            x1, y1 = face.right(), face.bottom()
+            cv2.rectangle(frame, (x, y), (x1, y1), (0, 0, 255), 2)
+            landmarks = predictor(gray, face)
+
+            left_point = (landmarks.part(36).x, landmarks.part(36).y)
+            right_point = (landmarks.part(39).x, landmarks.part(39).y)
+            top_center = midpoint(landmarks.part(37), landmarks.part(38))
+            bottom_center = midpoint(landmarks.part(41), landmarks.part(40))
+
+            #Gaze Ratio
+            left_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
+                                (landmarks.part(37).x, landmarks.part(37).y),
+                                (landmarks.part(38).x, landmarks.part(38).y),
+                                (landmarks.part(39).x, landmarks.part(39).y),
+                                (landmarks.part(40).x, landmarks.part(40).y),
+                                (landmarks.part(41).x, landmarks.part(41).y)], np.int32)
+            min_x = np.min(left_eye_region[:, 0])
+            max_x = np.max(left_eye_region[:, 0])
+            min_y = np.min(left_eye_region[:, 1])
+            max_y = np.max(left_eye_region[:, 1])
+
+            cv2.polylines(frame, [left_eye_region], True, (0, 255, 0), 2)
+
+
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
